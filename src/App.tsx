bca0d023e1346b264015103d4dc991fdfdb1bdd9
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2024-06-19 15:54:39 +0200

    tailwind is initialized and running

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <div className="h-full p8 text-slate-800 bg-gradient-from-r from-cyan-500 to-blue-500">
+        <h1 className="text-center text-5xl mb-4 font-display text-white">
+          APP
+        </h1>
+      </div>
     </>
   );
 }
